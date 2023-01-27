c7ac809b61e2215d015608d804dee73a7ba49d3c
Claquettes <math@claq.fr>
2023-01-27 10:15:44 +0100

    séparation des fonctions de collision et de mise à jour dans de nouveau fichiers

--- script.js
+++ script.js
@@ -84,85 +84,6 @@ function keyUpHandler(e) {
     }
 }
 
-function renderBall() {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = ball.color;
-    ctx.fill();
-}
-
-function renderPaddle() {
-    ctx.fillStyle = paddle.color;
-    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
-}
-
-// Render the bricks on the canvas
-function renderBricks() {
-    for (var i = 0; i < bricks.length; i++) {
-        ctx.fillStyle = bricks[i].color;
-        ctx.fillRect(bricks[i].x, bricks[i].y, bricks[i].width, bricks[i].height);
-        colorBricks();
-    }
-}
-
-function checkPaddleCollision() {
-    if (ball.y + ball.radius > paddle.y &&
-        ball.x > paddle.x &&
-        ball.x < paddle.x + paddle.width) {
-        ball.speedY = -ball.speedY;
-        var paddleCenter = paddle.x + paddle.width / 2;
-        var ballDistFromPaddleCenter = ball.x - paddleCenter;
-        ball.speedX = ballDistFromPaddleCenter * 0.15 + 0.5;
-    }
-}
-
-function checkBordersCollision() {
-    if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
-        ball.speedX = -ball.speedX;
-    }
-    if (ball.y - ball.radius < 0) {
-        ball.speedY = -ball.speedY;
-    }
-    //on check la collision du pad avec les bords du canvas
-    if (paddle.x < 0) {
-        paddle.x = 0;
-    }
-    if (paddle.x + paddle.width > canvas.width) {
-        paddle.x = canvas.width - paddle.width;
-    }
-}
-
-function checkBrickCollision() { //on check la collision avec les briques
-    for (var i = 0; i < bricks.length; i++) {
-        if ((ball.x > bricks[i].x) && (ball.x < bricks[i].x + bricks[i].width )&& (ball.y > bricks[i].y )&& (ball.y < bricks[i].y + bricks[i].height)) {
-            ball.speedY = -ball.speedY;
-            //on diminue la durabilité de la brique
-            bricks[i].durability -= 1;   
-            //si la durabilité est à 0, on la supprime
-            if (bricks[i].durability == 0) {
-            bricks.splice(i, 1);
-            }
-        }
-    }
-}
-
-function updateBallPosition() {
-    ball.x += ball.speedX + 0.1;
-    ball.y += ball.speedY + 0.1;
-    if (ball.speedX > maxSpeed) {
-        ball.speedX = maxSpeed;
-    }
-    if (ball.speedX < -maxSpeed) {
-        ball.speedX = -maxSpeed;
-    }
-    if (ball.speedY > maxSpeed) {
-        ball.speedY = maxSpeed;
-    }
-    if (ball.speedY < -maxSpeed) {
-        ball.speedY = -maxSpeed;
-    }
-}
-
 function checkGameOver() {
     if (ball.y - ball.radius > canvas.height) {
         alert("Game Over!");
@@ -183,15 +104,6 @@ function render() {
     renderBall();
     renderPaddle();
     renderBricks();
-}
-
-function updatePad(dt) {
-    dt= dt*100;
-    if (rightPressed) {
-        paddle.x += paddleSpeed * dt;
-    } else if (leftPressed) {
-        paddle.x -= paddleSpeed * dt;
-    }
 }
 
 function fixedUpdate() {
@@ -245,10 +157,6 @@ function checkURLParams() {
         bricks = Level2();
     }
 }
-
-
-
-
 
 start();
 
